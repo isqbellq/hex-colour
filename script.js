f018dc6236b45459c5bb99c821c4d0aa25948848
isqbellq <isabcdef@outlook.it>
2018-12-07 16:36:53 +0100

    minor fixes

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
  "use strict"
 
- let generateColour = function() {
+ function generateColourName() {
    let randomColour = '#' + Math.floor(Math.random() * 16777215).toString(16);
    return randomColour;
  }
 
- let printColour = function() {
-  let colour = generateColour(); 
+ function printColour() {
+  let colour = generateColourName(); 
   $(".display").css({"backgroundColor": colour});
   $(".colour-name").text(colour);
 }
